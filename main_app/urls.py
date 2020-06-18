7506d78c6d77e1ac11f0c6e443375480e9370d8f
connorj1002 <connorj1002@gmail.com>
2020-06-18 15:38:33 -0600

    adds auth post req to home route

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('wayfarer/', views.wayfarer_index, name='index'),
-  path('accounts/signup', views.signup, name='signup'),
   path('profile/', views.profile, name='profile')
 ]
