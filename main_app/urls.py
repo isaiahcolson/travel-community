311933b93c6cf7b8fa7d36e6a8b8264850a20259
connorj1002 <connorj1002@gmail.com>
2020-06-19 11:19:38 -0600

    login auth fixed

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('wayfarer/', views.wayfarer_index, name='index'),
-  path('profile/', views.profile, name='profile')
+  path('profile/', views.profile, name='profile'),
+  path('login/', views.user_login, name='user_login'),
 ]
