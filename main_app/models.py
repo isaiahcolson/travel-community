8c9d6d51ac6290a6d4305409d901fd7979815595
chanlathung1 <chanlathung@gmail.com>
2020-06-19 13:29:21 -0700

    submaster branch pull process

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class User:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, first_name, last_name, username, city, year, month, day):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.full_name = f"{first_name} {last_name}"
+    self.username = username
+    self.city = city
+    joined_date = datetime.datetime(year, month, day)
+    self.joined_date = (joined_date.strftime("%B %Y"))
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 import datetime
 
 from .forms import CreateUserForm
+from .models import User
+
+
 
 def home(request):
   error_message = ''
@@ -38,22 +41,7 @@ def user_login(request):
 def wayfarer_index(request):
   return render(request, 'wayfarer/index.html')
 
-
-
-class User:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, first_name, last_name, username, city, year, month, day):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.full_name = f"{first_name} {last_name}"
-    self.username = username
-    self.city = city
-    joined_date = datetime.datetime(year, month, day)
-    self.joined_date = (joined_date.strftime("%B %Y"))
-
-users = [
-  User('Goofy', 'Goof', 'goofus', 'San Francisco', 2020, 6, 19)
-]
-
 def profile(request):
+  users = User.objects.all()
   context = { 'users' : users }
   return render(request, 'profile.html', context)
