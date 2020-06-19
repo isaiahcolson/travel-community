2a305386ba6a6fda775b5e863bc3ea4121a4f27f
colsonisaiah <colson.isaiah@gmail.com>
2020-06-19 11:32:08 -0400

    added datetime to users date

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+import datetime
 
 from .forms import CreateUserForm
 
@@ -26,16 +27,17 @@ def wayfarer_index(request):
 
 
 class User:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, first_name, last_name, username, city, joined_date):
+  def __init__(self, first_name, last_name, username, city, year, month, day):
     self.first_name = first_name
     self.last_name = last_name
     self.full_name = f"{first_name} {last_name}"
     self.username = username
     self.city = city
-    self.joined_date = joined_date
+    joined_date = datetime.datetime(year, month, day)
+    self.joined_date = (joined_date.strftime("%B %Y"))
 
 users = [
-  User('Goofy', 'Goof', 'goofus', 'San Francisco', 4-4-2020)
+  User('Goofy', 'Goof', 'goofus', 'San Francisco', 2020, 6, 19)
 ]
 
 def profile(request):
