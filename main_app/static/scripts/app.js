94c0c6edc9947078659f7443713c92115ce8e944
colsonisaiah <colson.isaiah@gmail.com>
2020-06-19 00:26:39 -0400

    add transfer between login and signup modals

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -14,17 +14,30 @@ $('#signup-underlay').click(function() {
     $(document.body).css('overflow', 'visible');
 });
 
+$('.signup-login').click(function() {
+    $('#signup-modal').removeClass('modal-active');
+    $('#login-modal').addClass('modal-active');
+});
+
 
 
 /* login modal events */
 $('#login-modal-button').click(function() {
     $('#login-modal').addClass('modal-active');
+    $(document.body).css('overflow', 'hidden');
 });
 
 $('#login-close').click(function() {
     $('#login-modal').removeClass('modal-active');
+    $(document.body).css('overflow', 'visible');
 });
 
 $('#login-underlay').click(function() {
     $('#login-modal').removeClass('modal-active');
+    $(document.body).css('overflow', 'visible');
+});
+
+$('.login-signup').click(function() {
+    $('#login-modal').removeClass('modal-active');
+    $('#signup-modal').addClass('modal-active');
 });
