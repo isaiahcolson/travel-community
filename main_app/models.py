44dce5aaca447417fde63cb23e571b0ed19d1602
xscullark <xscullark@gmail.com>
2020-06-18 20:05:36 -0400

    models.py complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    city = models.CharField(max_length=15)
+    date = models.DateField(default=datetime.now(), blank=True)
+
+
+
+class City(models.Model):
+    city = models.CharField(max_length=15)
+    attractions = models.CharField(max_length=100)
+    review = models.CharField(max_length=450)
+    rating = models.IntegerField()
+
+class Post(models.Model):
+    name = models.CharField(max_length=100)
+    review = models.CharField(max_length=450)
+    rating = models.IntegerField()
+    date = models.DateField(default=datetime.now(), blank=True)
--- main_app/templates/models/models.py
+++
@@ -1,8 +0,0 @@
-class user(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
-    city = models.CharField(max_lenght=100)
-    review = models.CharField(max_length=300)
-    rating = models.IntegerField()
-
-    
