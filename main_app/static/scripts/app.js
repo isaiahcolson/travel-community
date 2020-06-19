17be77607eb476b30fdac1f5260633b8c666dcee
colsonisaiah <colson.isaiah@gmail.com>
2020-06-18 22:00:32 -0400

    fixed scrolling for modals

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,14 +1,17 @@
 /* signup modal events */
 $('#signup-modal-button').click(function() {
     $('#signup-modal').addClass('modal-active');
+    $(document.body).css('overflow', 'hidden');
 });
 
 $('#signup-close').click(function() {
     $('#signup-modal').removeClass('modal-active');
+    $(document.body).css('overflow', 'visible');
 });
 
 $('#signup-underlay').click(function() {
     $('#signup-modal').removeClass('modal-active');
+    $(document.body).css('overflow', 'visible');
 });
 
 
