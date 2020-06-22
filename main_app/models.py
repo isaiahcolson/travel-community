a1c64046afd742e20fdd6cabd453f1b47d5bbf46
connorj1002 <connorj1002@gmail.com>
2020-06-22 11:32:23 -0600

    adds User_post model

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,11 @@ import datetime
 class MyUser(models.Model):
     city = models.CharField(max_length=100, default='Add Your City')
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='MyUser')
+
+
+class User_Post(models.Model):
+  title = models.CharField(max_length=100)
+  city = models.CharField(max_length=100)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  date = models.DateField('Post Date')
+  content = models.TextField(max_length=1000)
