89d0e3b246b9ecd69215acc8d69e303e776d5101
connorj1002 <connorj1002@gmail.com>
2020-06-22 23:33:02 -0600

    adds C.R. to posts

--- main_app/forms.py
+++ main_app/forms.py
@@ -18,4 +18,4 @@ class EditUserForm(forms.ModelForm):
 class Post_Form(ModelForm):
     class Meta:
         model = User_Post
-        fields = ['title', 'date', 'city', 'content']
+        fields = ['title', 'city', 'content']
