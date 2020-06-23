ac5be8120ff4116a95b64b903e9d994529a824e4
chanlathung1 <chanlathung@gmail.com>
2020-06-23 03:19:23 -0700

    added City foreign-key to User_Post Model

--- main_app/models.py
+++ main_app/models.py
@@ -7,24 +7,27 @@ class MyUser(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='MyUser')
 
 
-# update city to a FK after City model creation
-class User_Post(models.Model):
-  title = models.CharField(max_length=100)
-  date = models.DateField('Post Date')
-  content = models.TextField(max_length=1000)
-  city = models.CharField(max_length=100)
-  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User_Posts')
-
-  def __str__(self):
-    return f"{self.get_title_display()} on {self.date}"
-
-  class Meta:
-    ordering = ['-date', 'city']
-
-
 class City(models.Model):
   name = models.CharField(max_length=100)
   country = models.CharField(max_length=100)
 
   def __str__(self):
     return self.name
+
+    
+# update city to a FK after City model creation
+class User_Post(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateField('Post Date')
+  content = models.TextField(max_length=1000)
+  city = models.ForeignKey(City, on_delete=models.CASCADE)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User_Posts')
+
+  def __str__(self):
+    return f"{self.get_title_display()} on {self.date}"
+
+
+class Meta:
+  ordering = ['-date', 'city']
+
+
